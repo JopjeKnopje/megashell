bb41db03dcc65cd1291c07e996220a69a40206a4
Joppe Boeve <jboeve@f1r5s16.codam.nl>
2023-07-25 12:27:32 +0200

    check if line is empty

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <marvin@42.fr>                       +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/07/24 12:00:23 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/07/24 16:23:24 by jboeve        ########   odam.nl         */
+/*   Updated: 2023/07/25 12:27:11 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/_types/_null.h>
 #include "libft.h"
+
 
 typedef enum e_cmd_name {
 	CMD_EXIT,
@@ -44,13 +47,22 @@ int main(int argc, char *argv[])
 	while (1) 
 	{
 		char *line = readline("megashell> ");
-		add_history(line);
+		rl_insert_text("123");
+		rl_redisplay();
+		rl_on_new_line();
+		if (ft_strisempty(line))
+		{
+			free(line);
+			continue;
+		}
 
+		add_history(line);
 		printf("line: [%s]\n", line);
 		if (is_cmd(CMD_EXIT, line))
 			exit(0);
 		else if (is_cmd(CMD_CLEAR_HIS, line))
 			rl_clear_history();
+		free(line);
 	}
 	
 	return 0;
