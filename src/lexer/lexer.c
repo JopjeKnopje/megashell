12e8fbfa180e691719b25161ec5d4b787e693f53
Joppe Boeve <jboeve@f1r5s21.codam.nl>
2023-08-03 18:00:04 +0200

    CAN FINALLY TOKENIZE QUOTES

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/07/31 16:57:13 by joppe         #+#    #+#                 */
-/*   Updated: 2023/08/03 12:18:48 by joppe         ########   odam.nl         */
+/*   Updated: 2023/08/03 17:59:50 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,23 @@ static t_token token_set(t_token_kind k, char *s, uint32_t len)
 	return (t);
 }
 
-static t_token lexer_tokenize_quote(char *s)
+static t_token lexer_tokenize_quote(char *s, char c)
 {
-	t_token		t;
-	uint32_t	i;
-	uint32_t	count;
+	t_token t;
+	uint32_t i = 1;
+	uint32_t len = ft_strlen(s);
 
-	i = 0;
-	count = 0;
-	while (s[i])
+	while (i < len)
 	{
-		if (s[i] == '\'')
-			count++;
+		if (s[i] == c)
+		{
+			t = token_set(TOKEN_QUOTE_SINGLE, s, i + 1);
+			return t;
+		}
 		i++;
 	}
 
-	if (count % 2)
-		return (token_set(TOKEN_ERROR, s, i));
-
-	i = 1;
-	while (s[i] && s[i] != '\'')
-	{
-		i++;
-	}
-	t = token_set(TOKEN_QUOTE_SINGLE, s, i + 1);
-	
-	return t;
+	return token_set(TOKEN_ERROR, s, 1);
 }
 
 static t_token lexer_next(t_lexer *l, char *s)
@@ -61,14 +52,12 @@ static t_token lexer_next(t_lexer *l, char *s)
 	t_token	t;
 
 	if (*s == '\'')
-		t = lexer_tokenize_quote(s);
+		t = lexer_tokenize_quote(s, *s);
 	else if (*s == '\"')
-		t = token_set(TOKEN_QUOTE_DOUBLE, s, 1);
+		t = lexer_tokenize_quote(s, *s);
 	else
 		t = token_set(TOKEN_UNKNOWN, s, 1);
 
-	if (t.kind == TOKEN_ERROR)
-		printf("Error\n");
 	return (t);
 }
 
@@ -78,11 +67,13 @@ void lexer(char *s)
 	t_token	t;
 
 	l.content = s;
+	printf("len %ld\n", ft_strlen(l.content));
 	while (*l.content)
 	{
-		t = lexer_next(&l, s);
-		printf("token_kind %s | token_content_len [%d] | token_content [%.*s]\n", TOKEN_NAMES[t.kind], t.content_len, t.content_len, t.content);
-		printf("lexer_content [%s]\n", l.content);
+		if (!(*l.content))
+			printf("IS NULL\n");
+		t = lexer_next(&l, l.content);
+		printf("token [%.*s]\n", t.content_len, t.content);
 		l.content += (t.content_len * sizeof(char));
 	}
 }
