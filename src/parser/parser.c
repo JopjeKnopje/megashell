d2a35e092af21f5709ce477ab645e86bc7750676
joppe <jboeve009@gmail.com>
2023-08-13 20:38:27 +0200

    added print token

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/08/12 23:18:28 by joppe         #+#    #+#                 */
-/*   Updated: 2023/08/13 20:18:29 by joppe         ########   odam.nl         */
+/*   Updated: 2023/08/13 20:32:32 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,10 @@ void pr_main(t_token_list *tok_list)
 	while (tmp)
 	{
 		t_token *t = &(tmp->content);
+
+		printf("\x1b[36;49m");
+		printf("token_kind %s | token_content [%.*s] | token_content_len [%d]\n", TOKEN_NAMES[t->kind], t->content_len, t->content, t->content_len);
+		printf("\x1b[0m");
 
 
 		tmp = tmp->next;
