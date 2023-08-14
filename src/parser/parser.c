b3ed15cdb2fd9e49c05df7b17c6080c02a1bf1d7
JopjeKnopje <jboeve009@gmail.com>
2023-08-14 10:34:09 +0200

    tryin to figure out how to dynamically store command_frames

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/08/12 23:18:28 by joppe         #+#    #+#                 */
-/*   Updated: 2023/08/14 10:20:25 by joppe         ########   odam.nl         */
+/*   Updated: 2023/08/14 10:32:46 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,26 @@
 #include "parser.h"
 #include <stddef.h>
 #include <stdio.h>
+
+static t_cf_list *pr_list_add_token(t_cf_list **cf_list, t_command_frame frame)
+{
+	t_cf_list	*node;
+
+	if (!cf_list)
+	{
+		*cf_list = pr_lstnew(frame);
+		if (!cf_list)
+			return (NULL);
+	}
+	else
+	{
+		node = pr_lstnew(frame);
+		if (!node)
+			return (NULL);
+		pr_lstadd_back(cf_list, node);
+	}
+	return (*cf_list);
+}
 
 void pr_main(t_token_list *tok_list)
 {
