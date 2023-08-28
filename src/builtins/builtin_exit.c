4b7f9ba5f29d6a98641a617abf7fb4cf4b68bc85
Iris Van Melsen <ivan-mel@f1r5s20.codam.nl>
2023-08-28 18:20:56 +0200

    builtins fixed I think

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: ivan-mel <ivan-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 16:11:11 by ivan-mel          #+#    #+#             */
-/*   Updated: 2023/08/22 14:41:43 by ivan-mel         ###   ########.fr       */
+/*   Updated: 2023/08/28 18:20:34 by ivan-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,23 @@ bool	builtin_run_exit(t_exec *execute, t_cmd_list *cmds)
 {
 	int	nb;
 
-// exit dan exit errorcode vorige
 	if (!cmds->content.argv[1])
 		exit(EXIT_SUCCESS);
 	nb = ft_atoi(cmds->content.argv[1]);
-// unless multiple digits with spaces
 	if (cmds->content.argv[2])
-		printf("Too Many Arguments\n");
+	{
+		if ((is_a_nb(cmds->content.argv[1]) && is_a_nb(cmds->content.argv[2]))
+			|| (!is_a_nb(cmds->content.argv[2])))
+		{
+			printf("exit: too many arguments\n");
+			exit(EXIT_FAILURE);
+		}
+		if (!is_a_nb(cmds->content.argv[1]) && is_a_nb(cmds->content.argv[2]))
+		{
+			printf("exit: a numeric argument required\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 	else if (!is_a_nb(cmds->content.argv[1]))
 		printf("Argument isn't a Number\n");
 	exit(nb);
