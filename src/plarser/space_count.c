9b45c9ca7cfdb5ec0badc2d8274a778ddff2fedf
Joppe Boeve <jboeve@f1r5s18.codam.nl>
2023-11-30 12:54:54 +0100

    Fix padding count

--- src/plarser/space_count.c
+++ src/plarser/space_count.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>            +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/11/29 14:13:28 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/11/29 17:18:52 by jboeve        ########   odam.nl         */
+/*   Updated: 2023/11/30 12:50:43 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t sc_count(t_token *t)
 	size_t i;
 
 	i = t->content_len;
-	while (t->content[i] && lx_is_metachar(t->content[i]))
+	while (t->content[i] && (!lx_is_metachar(t->content[i]) || t->content[i] == ' '))
 	{
 		i++;
 	}
@@ -30,14 +30,11 @@ size_t sc_count(t_token *t)
 t_tok_list *sc_main(t_tok_list *tokens)
 {
 	t_tok_list *head;
-	t_token_kind k;
 
 	head = tokens;
 	while (tokens)
 	{
-		k = tokens->token.kind;
-		if (k == TOKEN_TEXT || k == TOKEN_BLOCK_QUOTE_DOUBLE || k == TOKEN_BLOCK_QUOTE_SINGLE || k == TOKEN_BLOCK_DOLLAR)
-			tokens->token.padding = sc_count(&tokens->token);
+		tokens->token.padding = sc_count(&tokens->token);
 		tokens = tokens->next;
 	}
 	return head;
